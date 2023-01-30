8f1d09955872e0d2156c0a5e95c5aeb8232a0dcf
«Oksana <averinaoy@gmail.com>
2023-01-30 11:32:32 +0500

    IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter your name!");
+string username = Console.ReadLine();
+if(username == "Pekka")
+    Console.WriteLine("Kippis, tama on Pekka!");
+else
+    Console.Write("Terve, ");
+    Console.WriteLine(username);
